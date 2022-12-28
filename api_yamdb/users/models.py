0bb16a5bb1131ca20e8e262636ba3a9f860da7a8
Михаил Касаткин <m.m.kasatkin@gmail.com>
2022-12-28 22:26:26 +0300

    Working with users functionality is finished.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,15 @@
-from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+
+
+class CustomUserManager(UserManager):
+    def create_superuser(self, username, email=None, password=None,
+                         **extra_fields):
+        extra_fields.setdefault('role', 'admin')
+        extra_fields.setdefault('is_verified', True)
+        return super().create_superuser(username, email, password,
+                                        **extra_fields)
 
 
 class User(AbstractUser):
@@ -11,11 +20,14 @@ class User(AbstractUser):
     ROLE_CHOICES = ((value, role.capitalize()) for role, value in
                     USER_ROLES.items())
 
+    email = models.EmailField(blank=False, max_length=254)
     role = models.CharField(choices=ROLE_CHOICES,
                             default=USER_ROLES.get('USER'), max_length=50)
     is_verified = models.BooleanField(default=False)
 
     bio = models.TextField(blank=True, null=True)
+
+    objects = CustomUserManager()
 
     @property
     def is_admin(self):
